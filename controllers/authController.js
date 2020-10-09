548ba35f0989d647dead7685fa31ed67957b1462
sow4063 <sow4063@gmail.com>
2020-10-10 08:24:40 +0900

    add for secure and SIGTERM

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,18 +12,15 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.security = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+  });
 
   // Remove the password from the output
   user.password = undefined;
@@ -49,7 +46,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   console.log(url);
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -64,7 +61,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
 
-  createSendToken(user, 202, res);
+  createSendToken(user, 202, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -219,7 +216,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 3) Update changedPasswordAt property for the user : doing at userModel.pre('save')
   // 4) Log the user in, send JWT
-  createSendToken(user, 202, res);
+  createSendToken(user, 202, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -238,5 +235,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // User.findByIdAndUpdate will NOT work as intended!
 
   // 4) Log user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
