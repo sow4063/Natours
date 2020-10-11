d9894a708b5ba50c6856a43c75dc38003b415612
sow4063 <sow4063@gmail.com>
2020-10-11 16:45:11 +0900

    Improved stripe message

--- public/js/index.js
+++ public/js/index.js
@@ -68,3 +68,7 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
+  
